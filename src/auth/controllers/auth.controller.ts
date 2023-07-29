cff1e34dee3a05f478079a54c6a8d2ffe753ed5b
Lautaro Andres Saez <LautaroAndresSaez@gmail.com>
2023-07-29 11:25:02 -0300

    feat: create user

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from '../dtos/User.dtos';
+import { UsersService } from '../services/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  signUp(@Body() payload: CreateUserDto) {
+    return this.usersService.create(payload);
+  }
+}
--- src/auth/services/users.service.ts
+++ src/auth/services/users.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from '../dtos/User.dtos';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/User.entity';
+import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private SALT = 16;
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(payload: CreateUserDto): Promise<User> {
+    const { password, email } = payload;
+    const hashedPassword = bcrypt.hashSync(password, this.SALT);
+    const user = await this.userRepository.save({
+      ...payload,
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    });
+    delete user.password;
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+    delete user.password;
+    return user;
+  }
+
+  async findUserByEmail(email: string, password: string): Promise<User | null> {
+    const user = await this.userRepository.findOneBy({ email });
+    const isMatch = bcrypt.compareSync(password, user?.password);
+    if (!isMatch) return null;
+    delete user.password;
+    return user;
+  }
+
+  //TODO: implement update
+  update(id: string, payload: UpdateUserDto) {
+    throw new Error('This method is not implemented');
+  }
+}
